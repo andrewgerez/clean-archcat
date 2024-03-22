7b79219fff191d5a3f3d88dee216548912b3d6bb
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-22 15:06:05 -0300

    feat: prisma service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Post } from '@nestjs/common'
 import { AppService } from './app.service'
+import { PrismaService } from './prisma/prisma.service'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private appService: AppService,
+    private prisma: PrismaService
+  ) {}
 
-  @Get()
+  @Get('/')
   getHello(): string {
     return this.appService.getHello()
+  }
+
+  @Post('/prisma')
+  connection() {
+    return this.prisma.user.findMany()
   }
 }
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,24 @@
+import { Injectable,  OnModuleInit, OnModuleDestroy } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy 
+{
+  public client: PrismaClient
+
+  constructor() {
+    super({
+      log: ['warn', 'error'],
+    })
+  }
+
+  onModuleInit() {
+    return this.$connect()
+  }
+
+  onModuleDestroy() {
+    return this.$disconnect()
+  }
+}
