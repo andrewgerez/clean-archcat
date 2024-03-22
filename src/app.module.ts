0683b500b2cc196545b07f37da0dee5689b0adbb
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-22 18:53:09 -0300

    feat: create account controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { PrismaService } from './prisma/prisma.service'
+import { CreateAccountController } from './controllers/create-account.controller'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [CreateAccountController],
+  providers: [PrismaService],
 })
+
 export class AppModule {}
---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Post, HttpCode, Body, ConflictException } from "@nestjs/common"
+import { PrismaService } from "src/prisma/prisma.service"
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(
+    private prisma: PrismaService
+  ) {}
+
+  @Post()
+  @HttpCode(201)
+  async handle(@Body() body: any) {
+    const { name, email, password } = body
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      }
+    })
+
+    if (userWithSameEmail) {
+      throw new ConflictException('User with same e-mail address already exists.')
+    }
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      }
+    })
+  }
+}
