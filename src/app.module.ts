6403a28906f34ada215c6c9e53cefe850a96a0ba
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-23 01:06:11 -0300

    chore: nest passport and nest jwt setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,16 @@ import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
-  imports: [ConfigModule.forRoot({
+  imports: [
+  ConfigModule.forRoot({
     validate: env => envSchema.parse(env),
     isGlobal: true,
-  })],
+  }),
+  AuthModule,
+],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
