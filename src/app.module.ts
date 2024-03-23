afa38eb2f3560e2fc00a995f639aad9e9e832a80
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-23 15:47:34 -0300

    feat: create fetch recent questions controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateQuestionContorller } from './controllers/create-question.controller'
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { CreateQuestionContorller } from './controllers/create-question.controll
     CreateAccountController,
     AuthenticateController,
     CreateQuestionContorller,
+    FetchRecentQuestionsController,
   ],
   providers: [PrismaService],
 })
---
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Controller, UseGuards, Get, Query
+} from '@nestjs/common'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(
+    z.number().min(1)
+  )
+
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class FetchRecentQuestionsController {
+  constructor(
+    private prisma: PrismaService,
+  ) {}
+
+  @Get()
+  async handle(
+    @Query('page', queryValidationPipe) page: PageQueryParamSchema
+  ) {
+    const perPage = 1
+
+    const questions = await this.prisma.question.findMany({
+      take: perPage,
+      skip: (page - 1) * perPage,
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+
+    return {
+      questions,
+    }
+  }
+}
