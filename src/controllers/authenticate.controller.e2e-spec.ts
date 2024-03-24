cada0a082fd30e89ebe7079c4c54fb86745ef0b4
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-23 22:19:16 -0300

    test: e2e authenticate test

---
+++ src/controllers/authenticate.controller.e2e-spec.ts
@@ -0,0 +1,45 @@
+import { AppModule } from '@/app.module'
+import { Test } from '@nestjs/testing'
+import { INestApplication } from '@nestjs/common'
+import request from 'supertest'
+import { test } from 'vitest'
+import { PrismaService } from '@/prisma/prisma.service'
+import { hash } from 'bcryptjs'
+
+describe('E2E: Authenticate', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+
+  test('[POST] /sessions', async () => {
+    await prisma.user.create({
+      data: {
+        name: 'Andrew Gerez',
+        email: 'email@email.com',
+        password: await hash('123456', 8),
+      }
+    })
+
+    const response = await request(app.getHttpServer()).post('/sessions').send({
+      email: 'email@email.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toBe(201)
+    expect(response.body).toEqual({
+      access_token: expect.any(String)
+    })
+  })
+})
