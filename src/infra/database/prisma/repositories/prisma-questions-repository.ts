b4f117dd7b576a43873d9645f0a59e219d2ee3e3
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-24 17:58:28 -0300

    feat: creates the function to find question by slug

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -24,8 +24,18 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
 
     return PrismaQuestionMapper.toDomain(question)
   }
-  findBySlug(slug: string): Promise<Question> {
-    throw new Error('Method not implemented.');
+  async findBySlug(slug: string): Promise<Question> {
+    const question = await this.prisma.question.findUnique({
+      where: {
+        slug,
+      }
+    })
+
+    if (!question) {
+      return null
+    }
+
+    return PrismaQuestionMapper.toDomain(question)
   }
   findManyRecent(params: PaginationParams): Promise<Question[]> {
     throw new Error('Method not implemented.');
