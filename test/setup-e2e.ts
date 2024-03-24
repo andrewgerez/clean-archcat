3b553f7873c582ab90eb8793056b9cec1a2caaa4
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-23 21:52:43 -0300

    feat: isolated schemas in the database for e2e testing

---
+++ test/setup-e2e.ts
@@ -0,0 +1,33 @@
+import 'dotenv/config'
+
+import { PrismaClient } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
+
+const prisma = new PrismaClient()
+const schemaId = randomUUID()
+
+function generateUniqueDatabaseURL() {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schemaId)
+
+  return url.toString()
+}
+
+beforeAll(async () => {
+  const databaseURL = generateUniqueDatabaseURL()
+
+  process.env.DATABASE_URL = databaseURL
+
+  execSync('pnpm prisma migrate deploy')
+})
+
+afterAll(async () => {
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
+  await prisma.$disconnect()
+})
