40e6d9dd01edbf9932edb78155f2428dab127ee0
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-23 22:15:20 -0300

    test: e2e create account test

--- src/controllers/create-account.controller.e2e-spec.ts
+++ src/controllers/create-account.controller.e2e-spec.ts
@@ -1,1 +1,42 @@
-test('', () => {})
+import { AppModule } from '@/app.module'
+import { Test } from '@nestjs/testing'
+import { INestApplication } from '@nestjs/common'
+import request from 'supertest'
+import { test } from 'vitest'
+import { PrismaService } from '@/prisma/prisma.service'
+
+describe('E2E: Create account', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+
+  test('[POST] /accounts', async () => {
+    const response = await request(app.getHttpServer()).post('/accounts').send({
+      name: 'Andrew Gerez',
+      email: 'email@email.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toBe(201)
+
+    const userOnDatabase = await prisma.user.findUnique({
+      where: {
+        email: 'email@email.com',
+      }
+    })
+
+    expect(userOnDatabase).toBeTruthy()
+  })
+})
