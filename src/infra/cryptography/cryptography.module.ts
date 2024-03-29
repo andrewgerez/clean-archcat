b9b797456e241d9066cdf9de182b811f8feb42df
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-29 18:49:35 -0300

    feat: providers e exports in cryptography module

--- src/infra/cryptography/cryptography.module.ts
+++ src/infra/cryptography/cryptography.module.ts
@@ -1,4 +1,16 @@
-import { Module } from "@nestjs/common";
+import { Module } from '@nestjs/common'
+import { Encrypter } from '@/domain/forum/application/cryptography/encrypter'
+import { HashGenerator } from '@/domain/forum/application/cryptography/hash-generator'
+import { HashComparer } from '@/domain/forum/application/cryptography/hash-comparer'
+import { JwtEncrypter } from './jwt-encrypter'
+import { BcryptHasher } from './bcrypt-hasher'
 
-@Module({})
-export class CryptographyModule {}
+@Module({
+  providers: [
+    { provide: Encrypter, useClass: JwtEncrypter },
+    { provide: HashGenerator, useClass: BcryptHasher },
+    { provide: HashComparer, useClass: BcryptHasher },
+  ],
+  exports: [Encrypter, HashGenerator, HashComparer]
+})
+export class CryptographyModule { }
