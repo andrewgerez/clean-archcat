f40c1e39bfd2d1db5cb9b63f1492cd3f5cc3726d
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-30 00:17:22 -0300

    feat: error handling in authenticate controller

--- src/infra/http/controllers/authenticate.controller.ts
+++ src/infra/http/controllers/authenticate.controller.ts
@@ -1,12 +1,14 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   HttpCode,
-  Post, UsePipes
+  Post, UnauthorizedException, UsePipes
 } from '@nestjs/common'
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
 import { z } from 'zod'
 import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student'
+import { WrongCredentialsError } from '@/domain/forum/application/use-cases/errors/wrong-credentials-error'
 
 const authenticateBodySchema = z.object({
   email: z.string().email(),
@@ -30,7 +32,14 @@ export class AuthenticateController {
     })
 
     if (result.isLeft()) {
-      throw new Error()
+      const error = result.value
+
+      switch (error.constructor) {
+        case WrongCredentialsError:
+          throw new UnauthorizedException(error.message)
+        default:
+          throw new BadRequestException(error.message)
+      }
     }
 
     const { accessToken } = result.value
@@ -40,4 +49,3 @@ export class AuthenticateController {
     }
   }
 }
- 
