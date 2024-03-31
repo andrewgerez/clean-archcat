ce856def3d71ff76d5e0ea5370f8c9c2649cb8ae
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-31 00:39:34 -0300

    feat: create prisma question comment mapper

---
+++ src/infra/database/prisma/mappers/prisma-question-comment-mapper.ts
@@ -0,0 +1,34 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
+import { Comment as PrismaComment, Prisma } from '@prisma/client'
+
+export class PrismaQuestionCommentMapper {
+  static toDomain(raw: PrismaComment): QuestionComment {
+    if (!raw.questionId) {
+      throw new Error('Invalid comment type.')
+    }
+
+    return QuestionComment.create(
+      {
+        content: raw.content,
+        authorId: new UniqueEntityID(raw.authorId),
+        questionId: new UniqueEntityID(raw.questionId),
+        createdAt: raw.createdAt,
+        updatedAt: raw.updateAt
+      },
+      new UniqueEntityID(raw.id))
+  }
+
+  static toPersistence(
+    questionComment: QuestionComment
+  ): Prisma.CommentUncheckedCreateInput {
+    return {
+      id: questionComment.id.toString(),
+      authorId: questionComment.authorId.toString(),
+      questionId: questionComment.questionId.toString(),
+      content: questionComment.content,
+      createdAt: questionComment.createdAt,
+      updateAt: questionComment.updatedAt,
+    }
+  }
+}
