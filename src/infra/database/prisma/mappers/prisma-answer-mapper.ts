86142f5e50b5177866f372169ac396ceeaf84ebf
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-31 00:31:13 -0300

    feat: create prisma answer mapper

---
+++ src/infra/database/prisma/mappers/prisma-answer-mapper.ts
@@ -0,0 +1,28 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Answer } from '@/domain/forum/enterprise/entities/answer'
+import { Answer as PrismaAnswer, Prisma } from '@prisma/client'
+
+export class PrismaAnswerMapper {
+  static toDomain(raw: PrismaAnswer): Answer {
+    return Answer.create(
+      {
+        content: raw.content,
+        authorId: new UniqueEntityID(raw.authorId),
+        questionId: new UniqueEntityID(raw.questionId),
+        createdAt: raw.createdAt,
+        updatedAt: raw.updateAt
+      },
+      new UniqueEntityID(raw.id))
+  }
+
+  static toPersistence(answer: Answer): Prisma.AnswerUncheckedCreateInput {
+    return {
+      id: answer.id.toString(),
+      authorId: answer.authorId.toString(),
+      questionId: answer.questionId.toString(),
+      content: answer.content,
+      createdAt: answer.createdAt,
+      updateAt: answer.updatedAt,
+    }
+  }
+}
