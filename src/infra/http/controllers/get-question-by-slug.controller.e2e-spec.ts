596f1e888465b380ac5c95449e4a74bf559f1470
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-31 15:34:46 -0300

    refactory: use studentFactory in e2e test

--- src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
+++ src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
@@ -4,45 +4,42 @@ import { INestApplication } from '@nestjs/common'
 import request from 'supertest'
 import { test } from 'vitest'
 import { PrismaService } from '@/infra/database/prisma/prisma.service'
-import { hash } from 'bcryptjs'
 import { JwtService } from '@nestjs/jwt'
+import { StudentFactory } from 'test/factories/make-student'
+import { DatabaseModule } from '@/infra/database/database.module'
 
 describe('E2E: Get question by slug', () => {
   let app: INestApplication
   let prisma: PrismaService
+  let studentFactory: StudentFactory
   let jwt: JwtService
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory],
     }).compile()
 
     app = moduleRef.createNestApplication()
 
     prisma = moduleRef.get(PrismaService)
-
+    studentFactory = moduleRef.get(StudentFactory)
     jwt = moduleRef.get(JwtService)
 
     await app.init()
   })
 
   test('[GET] /questions/:slug', async () => {
-    const user = await prisma.user.create({
-      data: {
-        name: 'Andrew Gerez',
-        email: 'email@email.com',
-        password: await hash('123456', 8),
-      },
-    })
+    const user = await studentFactory.makePrismaStudent()
 
-    const acessToken = jwt.sign({ sub: user.id })
+    const acessToken = jwt.sign({ sub: user.id.toString() })
 
     await prisma.question.create({
       data: {
         title: 'Question 01',
         slug: 'question-01',
         content: 'Some content',
-        authorId: user.id,
+        authorId: user.id.toString(),
       },
     })
 
--- test/factories/make-student.ts
+++ test/factories/make-student.ts
@@ -2,6 +2,9 @@ import { faker } from '@faker-js/faker'
 
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Student, StudentProps } from '@/domain/forum/enterprise/entities/student'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { PrismaStudentMapper } from '@/infra/database/prisma/mappers/prisma-student-mapper'
 
 export function makeStudent(
   override: Partial<StudentProps> = {},
@@ -18,4 +21,19 @@ export function makeStudent(
   )
 
   return question
+}
+
+@Injectable()
+export class StudentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaStudent(data: Partial<StudentProps> = {}): Promise<Student> {
+    const student = makeStudent(data)
+
+    await this.prisma.user.create({
+      data: PrismaStudentMapper.toPrisma(student)
+    })
+
+    return student
+  }
 }
