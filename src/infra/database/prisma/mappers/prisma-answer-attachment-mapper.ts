a53e1246e927dd683a79904639ae66f9f2b5f4b4
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-31 00:45:24 -0300

    feat: create prisma answer attachment mapper

---
+++ src/infra/database/prisma/mappers/prisma-answer-attachment-mapper.ts
@@ -0,0 +1,18 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { AnswerAttachment } from '@/domain/forum/enterprise/entities/answer-attachment'
+import { Attachment as PrismaAttachment } from '@prisma/client'
+
+export class PrismaAnswerAttachmentMapper {
+  static toDomain(raw: PrismaAttachment): AnswerAttachment {
+    if (!raw.answerId) {
+      throw new Error('Invalid attachment type.')
+    }
+
+    return AnswerAttachment.create(
+      {
+        attachmentId: new UniqueEntityID(raw.id),
+        answerId: new UniqueEntityID(raw.questionId),
+      },
+      new UniqueEntityID(raw.id))
+  }
+}
