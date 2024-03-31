45bdac848db09ee433251937537e6e462482fc8e
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-31 19:48:40 -0300

    test: e2e delete answer test

---
+++ src/infra/http/controllers/delete-answer.controller.e2e-spec.ts
@@ -0,0 +1,72 @@
+import { AppModule } from '@/infra/app.module'
+import { Test } from '@nestjs/testing'
+import { INestApplication } from '@nestjs/common'
+import request from 'supertest'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { JwtService } from '@nestjs/jwt'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { StudentFactory } from 'test/factories/make-student'
+import { QuestionFactory } from 'test/factories/make-question'
+import { AnswerFactory } from 'test/factories/make-answer'
+
+describe('E2E: Delete answer', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
+  let answerFactory: AnswerFactory
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [
+        StudentFactory,
+        QuestionFactory,
+        AnswerFactory,
+      ]
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+    studentFactory = moduleRef.get(StudentFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
+    answerFactory = moduleRef.get(AnswerFactory)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[DELETE] /answers/:id', async () => {
+    const user = await studentFactory.makePrismaStudent()
+
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+    })
+
+    const answer = await answerFactory.makePrismaAnswer({
+      authorId: user.id,
+      questionId: question.id,
+    })
+
+    const answerId = answer.id.toString()
+
+    const acessToken = jwt.sign({ sub: user.id.toString() })
+
+    const response = await request(app.getHttpServer())
+      .delete(`/answers/${answer.id}`)
+      .set('Authorization', `Bearer ${acessToken}`)
+      .send()
+
+    expect(response.statusCode).toBe(204)
+
+    const answerOnDatabase = await prisma.question.findUnique({
+      where: {
+        id: answerId,
+      },
+    })
+
+    expect(answerOnDatabase).toBeNull()
+  })
+})
