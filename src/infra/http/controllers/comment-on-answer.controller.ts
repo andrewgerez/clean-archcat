f58e4d07aadd5240eadec2d5b71d2ede946255f3
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-31 23:40:53 -0300

    feat: create comment on answer controller

---
+++ src/infra/http/controllers/comment-on-answer.controller.ts
@@ -0,0 +1,39 @@
+import { Controller, Post, Body, BadRequestException, Param } from '@nestjs/common'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { z } from 'zod'
+import { CommentOnAnswerUseCase } from '@/domain/forum/application/use-cases/comment-on-answer'
+
+const commentOnAnswerBodySchema = z.object({
+  content: z.string(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(commentOnAnswerBodySchema)
+
+type CommentOnAnswerBodySchema = z.infer<typeof commentOnAnswerBodySchema>
+
+@Controller('/answers/:answerId/comments')
+export class CommentOnAnswerController {
+  constructor(private commentOnAnswer: CommentOnAnswerUseCase) {}
+
+  @Post()
+  async handle(
+    @Body(bodyValidationPipe) body: CommentOnAnswerBodySchema,
+    @CurrentUser() user: UserPayload,
+    @Param('answerId') answerId: string,
+  ) {
+    const { content } = body
+    const { sub: userId } = user
+
+    const result = await this.commentOnAnswer.execute({
+      content,
+      authorId: userId,
+      answerId,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
