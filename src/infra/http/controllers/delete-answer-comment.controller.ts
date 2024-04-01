afb5cf1dcfcc26256599a4c94b0574d3ad94a3be
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-31 23:45:55 -0300

    feat: create delete answer comment controller

---
+++ src/infra/http/controllers/delete-answer-comment.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, BadRequestException, Delete, HttpCode, Param } from '@nestjs/common'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import { DeleteAnswerCommentUseCase } from '@/domain/forum/application/use-cases/delete-answer-comment'
+
+@Controller('/answers/comments/:id')
+export class DeleteAnswerCommentController {
+  constructor(private deleteAnswerComment: DeleteAnswerCommentUseCase) {}
+
+  @Delete()
+  @HttpCode(204)
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Param('id') answerCommentId: string
+  ) {
+    const { sub: userId } = user
+
+    const result = await this.deleteAnswerComment.execute({
+      authorId: userId,
+      answerCommentId,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
