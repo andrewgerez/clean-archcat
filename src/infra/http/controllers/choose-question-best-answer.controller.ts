035b310fcee0b4df26c37cef36c7e9ba43af93b8
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-31 22:54:26 -0300

    feat: create choose question best answer controller

---
+++ src/infra/http/controllers/choose-question-best-answer.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, BadRequestException, HttpCode, Param, Patch } from '@nestjs/common'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import { ChooseQuestionBestAnswerUseCase } from '@/domain/forum/application/use-cases/choose-question-best-answer'
+
+@Controller('/answers/:answerId/choose-as-best')
+export class ChooseQuestionBestAnswerController {
+  constructor(private chooseQuestionBestAnswer: ChooseQuestionBestAnswerUseCase) {}
+
+  @Patch()
+  @HttpCode(204)
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Param('answerId') answerId: string
+  ) {
+    const { sub: userId } = user
+
+    const result = await this.chooseQuestionBestAnswer.execute({
+      authorId: userId,
+      answerId,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
