e7c4636087b4c832d8a516ddc96235caff8211fd
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-31 23:20:20 -0300

    feat: create comment on question controller

---
+++ src/infra/http/controllers/comment-on-question.controller.ts
@@ -0,0 +1,39 @@
+import { Controller, Post, Body, BadRequestException, Param } from '@nestjs/common'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { z } from 'zod'
+import { CommentOnQuestionUseCase } from '@/domain/forum/application/use-cases/comment-on-question'
+
+const commentOnQuestionBodySchema = z.object({
+  content: z.string(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(commentOnQuestionBodySchema)
+
+type CommentOnQuestionBodySchema = z.infer<typeof commentOnQuestionBodySchema>
+
+@Controller('/questions/:questionId/comments')
+export class CommentOnQuestionController {
+  constructor(private commentOnQuestion: CommentOnQuestionUseCase) {}
+
+  @Post()
+  async handle(
+    @Body(bodyValidationPipe) body: CommentOnQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+    @Param('questionId') questionId: string,
+  ) {
+    const { content } = body
+    const { sub: userId } = user
+
+    const result = await this.commentOnQuestion.execute({
+      content,
+      authorId: userId,
+      questionId,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
