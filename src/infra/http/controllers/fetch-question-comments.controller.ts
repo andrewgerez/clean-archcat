65f340a418c1ea8eb6d36e9c4145967b2117144b
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-31 23:55:10 -0300

    feat: create fetch question comments controller

---
+++ src/infra/http/controllers/fetch-question-comments.controller.ts
@@ -0,0 +1,42 @@
+import { Controller, Get, Query, BadRequestException, Param } from '@nestjs/common'
+import { z } from 'zod'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { FetchQuestionCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-question-comments'
+import { CommentPresenter } from '../presenters/comment-presenter'
+
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+
+@Controller('/questions/:questionId/comments')
+export class FetchQuestionCommentsController {
+  constructor(private fetchQuestionComments: FetchQuestionCommentsUseCase) {}
+
+  @Get()
+  async handle(
+    @Query('page', queryValidationPipe) page: PageQueryParamSchema,
+    @Param('questionId') questionId: string
+  ) {
+    const result = await this.fetchQuestionComments.execute({
+      questionId,
+      page,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+
+    const questionComments = result.value.questionComments
+
+    return {
+      questionComments: questionComments.map(CommentPresenter.toHTTP),
+    }
+  }
+}
