e3398b3e61bb7fccdef15b0b8981aba8a71b74c4
Andrew Gerez <andrewgerez18@gmail.com>
2024-04-10 21:01:35 -0300

    feat: create many and delete many methods in question attachments repository

--- src/domain/forum/application/repositories/question-attachments-repository.ts
+++ src/domain/forum/application/repositories/question-attachments-repository.ts
@@ -1,6 +1,8 @@
 import { QuestionAttachment } from '../../enterprise/entities/question-attachment'
 
 export abstract class QuestionAttachmentsRepository {
+  abstract createMany(attachments: QuestionAttachment[]): Promise<void>
+  abstract deleteMany(attachments: QuestionAttachment[]): Promise<void>
   abstract findManyByQuestionId(questionId: string): Promise<QuestionAttachment[]>
   abstract deleteManyByQuestionId(questionId: string): Promise<void>
 }
