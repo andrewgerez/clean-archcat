cec46c8c2166f54b8bff190b248c05cd4304e9e9
Andrew Gerez <andrewgerez18@gmail.com>
2024-04-10 21:03:58 -0300

    feat: implements new methods in in memory question attachments repository

--- test/repositories/in-memory-question-attachments-repository.ts
+++ test/repositories/in-memory-question-attachments-repository.ts
@@ -6,6 +6,20 @@ export class InMemoryQuestionAttachmentsRepository
 {
   public items: QuestionAttachment[] = []
 
+  async createMany(attachments: QuestionAttachment[]): Promise<void> {
+    this.items.push(...attachments)
+  }
+
+  async deleteMany(attachments: QuestionAttachment[]): Promise<void> {
+    const questionAttachments = this.items = this.items.filter(
+      (item) => {
+        return !attachments.some((attachment) => attachment.equals(item))
+      }
+    )
+
+    this.items = questionAttachments
+  }
+
   async findManyByQuestionId(questionId: string) {
     const questionAttachments = this.items.filter(
       (item) => item.questionId.toString() === questionId,
