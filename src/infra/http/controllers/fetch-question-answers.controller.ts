22c4008569b1b63805f8278541c55bb0a445f5ac
Andrew Gerez <andrewgerez18@gmail.com>
2024-04-13 21:48:55 -0300

    feat: create question details value-object

--- src/infra/http/controllers/fetch-question-answers.controller.ts
+++ src/infra/http/controllers/fetch-question-answers.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Query, BadRequestException, Param } from '@nestjs/comm
 import { z } from 'zod'
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
 import { FetchQuestionAnswersUseCase } from '@/domain/forum/application/use-cases/fetch-question-answers'
-import { QuestionPresenter } from '../presenters/question-presenter'
+  import { QuestionPresenter } from '../presenters/question-presenter'
 import { AnswerPresenter } from '../presenters/answer-presenter'
 
 const pageQueryParamSchema = z
